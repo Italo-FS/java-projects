6704e52e1837ae6f2068036d2b9c9235df721bb6
Italo Fontenele <italo.fontenele.silva@gmail.com>
2023-08-23 11:56:10 -0300

    rosolução do problema 1397 - Jogo do Maior

---
+++ Estrutura de Dados/LISTA DE EXERCÍCIOS 1/1397 - Jogo do Maior/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+
+    while (true) {
+      int rodadas = Integer.parseInt(sc.nextLine());
+
+      if (rodadas == 0) break;
+
+      int pa = 0;
+      int pb = 0;
+
+      for (int i = 0; i < rodadas; i++) {
+        String[] input = sc.nextLine().split(" ");
+
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+
+        if (a > b) pa++;
+        if (a < b) pb++;
+      }
+
+      System.out.printf("%d %d%n", pa, pb);
+    }
+  }
+}
