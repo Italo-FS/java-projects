1af944debec2b8d07d6a5d91e9d1b59d139195a7
Italo Fontenele <italo.fontenele.silva@gmail.com>
2023-08-23 11:10:07 -0300

    rosolução do problema 1073 - Quadrado de Pares

---
+++ Estrutura de Dados/LISTA DE EXERCÍCIOS 1/1073 - Quadrado de Pares/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+
+    int n = Integer.parseInt(sc.nextLine());
+
+    for (int i = 2; i <= n; i += 2) {
+      System.out.printf("%d^2 = %.0f%n", i, Math.pow(i, 2));
+    }
+
+    sc.close();
+  }
+}
