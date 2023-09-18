f0dbbe6bf10dd9b63833a040df6042ac5272e423
Italo Fontenele <italo.fontenele.silva@gmail.com>
2023-09-18 11:54:55 -0300

    18/09/2023

---
+++ Estrutura de Dados/Listas/src/DinamicStructures/DinamicStack.java
@@ -0,0 +1,7 @@
+package DinamicStructures;
+
+public class DinamicStack {
+	public int height;
+	private Node<T> head;
+	private Node<T> tail;
+}
---
+++ Estrutura de Dados/Listas/src/DinamicStructures/Node.java
@@ -0,0 +1,13 @@
+package DinamicStructures;
+
+public class Node<T> {
+	T value;
+	Node<T> next;
+	Node<T> previous;
+
+	public Node(T value) {
+		this.value = value;
+		this.next = null;
+		this.previous = null;
+	}
+}
