6f066527c19cec01ec28550684dd97dc547944bf
Italo Fontenele <italo.fontenele.silva@gmail.com>
2023-12-05 14:24:09 -0300

    correção ortográfica

--- Estrutura de Dados/AV3/src/App.java
+++ Estrutura de Dados/AV3/src/App.java
@@ -22,15 +22,17 @@ public class App {
     }
   }
 }
-// == Passo a Passo ============================================================
-// 1. Lê o arquivo .txt e armazena seu conteúdo na variável 'fileContent'.
-// 2. A cada linha de 'fileContent':
-//    1. Cria uma Pilha de Árvores (treeStack).
-//    2. Empilha cada palavra de 'fileContent' na Pilha de Palavras (wordStack).
-//    3. Empurra uma nova Árvore de Strings no topo da Pilha de Árvores 'treeStack'.
-//    4. Desempilha cada palavra de 'wordStack' equanto às insere na Árvore de
-//       Strings que está no topo da Pilha de Ávores (treeStack).
-// 3. A cada arvore da Pilha de Árvores (treeStack):
-//    1. Desempilha cada Árvore enquando aplica o hash na mesma.
-//    2. Imprime o hash da Árvore no console.
-// ==============================================================================
+/**
+ * == Passo a Passo ============================================================
+ * 1. Lê o arquivo .txt e armazena seu conteúdo na variável 'fileContent'.
+ * 2. Para cada linha em 'fileContent':
+ *    1. Cria uma Pilha de Árvores (treeStack).
+ *    2. Empilha cada palavra da linha na Pilha de Palavras (wordStack).
+ *    3. Empilha uma nova Árvore de Strings no topo da Pilha de Árvores 'treeStack'.
+ *    4. Desempilha cada palavra de 'wordStack' e insere na Árvore de Strings no topo
+ *       da Pilha de Árvores (treeStack).
+ * 3. Para cada árvore na Pilha de Árvores (treeStack):
+ *    1. Desempilha cada Árvore e aplica o hash.
+ *    2. Imprime o hash da Árvore no console.
+ * ==============================================================================
+ **/
